2d76b41a3dfeadeead038997dfaec7e814c6579d
Mohit Rajpurohit <mrmohitrajgor9414@gmail.com>
2023-06-12 16:31:00 +0530

    added pick an image input

--- favourite_place_app/lib/screen/add_place.dart
+++ favourite_place_app/lib/screen/add_place.dart
@@ -1,4 +1,5 @@
 import 'package:favourite_place_app/provider/user_place.dart';
+import 'package:favourite_place_app/widget/image_input.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -29,6 +30,8 @@ class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
               const SizedBox(
                 height: 20,
               ),
+              ImageInput(),
+              SizedBox(height: 10,),
               ElevatedButton.icon(
                 icon: const Icon(Icons.add),
                 onPressed: _savePlace,
