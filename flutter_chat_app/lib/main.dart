33fa2a137371d2f6fb7d42f976424ce17f312403
Mohit Rajpurohit <mrmohitrajgor9414@gmail.com>
2023-06-15 18:40:13 +0530

    Flutter Chat App Setup

---
+++ flutter_chat_app/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_chat_app/screen/my_home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.red,
+        ),
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
---
+++ flutter_chat_app/lib/screen/my_home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.primaryContainer,
+        title: const Text("Chat App"),
+      ),
+      body: const Center(child: Text("Flutter Chat App"),),
+    );
+  }
+}
