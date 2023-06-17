174a525b4860e878b67041fe1321b3ac96a8060a
Mohit Rajpurohit <mrmohitrajgor9414@gmail.com>
2023-06-17 12:00:56 +0530

    Firebase CLI and SDK setup
    Command-line-interface
    Software-development-kit

--- flutter_chat_app/lib/main.dart
+++ flutter_chat_app/lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_chat_app/screen/auth_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:flutter_chat_app/screen/splash_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
